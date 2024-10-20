699ab813d2c76bd2474a6ab3085f9903af84a8b4
Murali Krishna <mural.krish4u@gmail.com>
2024-10-20 15:25:40 +0530

    Nave bar Implemented

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import {
+  Flex,
+  HStack,
+  Image,
+  Link,
+  Spacer,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
+import vmUniversity from "../assets/vm-university.png";
+
+const NavBar = () => {
+  return (
+    <HStack color="black">
+      <Flex align="center" gap="12px">
+        <Image src={vmUniversity} boxSize="60px" />
+        <Text fontSize="2xl" color="tomato" as="b">
+          VM University
+        </Text>
+      </Flex>
+      <Spacer />
+      <HStack
+        align="center"
+        gap="4"
+        divider={<StackDivider borderColor="gray.800" />}
+      >
+        <Link href="#">HOME</Link>
+        <Link href="#">COURSES</Link>
+        <Link href="#">CERTIFICATES</Link>
+        <Link href="#">HELP</Link>
+        <Link href="#">LOGIN</Link>
+      </HStack>
+    </HStack>
+  );
+};
+
+export default NavBar;
