14a8363d0b7085df826ed7368a1199e8a32c485a
Murali Krishna <mural.krish4u@gmail.com>
2024-10-20 19:24:34 +0530

    Replaced Flex layout with Container in NavBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
-import ImageCycler from './components/ImageCycler';
+import ImageCycler from "./components/ImageCycler";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Grid templateAreas={{ base: `"nav" "body" "footer"` }} bg="white">
-      <GridItem area="nav">
+      <GridItem area="nav" bg="blue.600">
         <NavBar />
       </GridItem>
 
-      <GridItem area="body">
+      <GridItem area="body" bg="white">
         <ImageCycler interval={5000} />
       </GridItem>
 
-      <GridItem area="footer" bg="red.600">
-        Footer
+      <GridItem area="footer" bg="blue.600">
+        <Footer />
       </GridItem>
     </Grid>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import {
+  Container,
   Flex,
   HStack,
   Image,
   Link,
   Spacer,
+  Stack,
   StackDivider,
   Text,
 } from "@chakra-ui/react";
@@ -11,26 +13,33 @@ import vmUniversity from "../assets/vm-university.png";
 
 const NavBar = () => {
   return (
-    <HStack color="black" px={10} py={2}>
-      <Flex align="center" gap="12px">
+    <Container
+      as={Stack}
+      maxW={"6xl"}
+      py={4}
+      direction={{ base: "column", md: "row" }}
+      spacing={4}
+      justify={{ base: "center", md: "space-between" }}
+      align={{ base: "center", md: "center" }}
+    >
+      <Stack direction={"row"} align="center">
         <Image src={vmUniversity} boxSize="60px" />
         <Text fontSize="2xl" color="tomato" as="b">
           VM University
         </Text>
-      </Flex>
-      <Spacer />
-      <HStack
-        align="center"
+      </Stack>
+      <Stack
+        direction={"row"}
         gap="4"
-        divider={<StackDivider borderColor="gray.800" />}
+        divider={<StackDivider borderColor="white" />}
       >
         <Link href="#">HOME</Link>
         <Link href="#">COURSES</Link>
         <Link href="#">CERTIFICATES</Link>
         <Link href="#">HELP</Link>
         <Link href="#">LOGIN</Link>
-      </HStack>
-    </HStack>
+      </Stack>
+    </Container>
   );
 };
 
